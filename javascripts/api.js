68d0133651a2f621220bfe5f7ec5ca79854f054f
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-18 15:54:58 -0700

    clean up api.js

--- javascripts/api.js
+++ javascripts/api.js
@@ -1,6 +1,13 @@
 const BASE_URL = 'https://api.github.com/';
 const USER_NAME = 'daniel-covelli';
 
+/**
+ * gets an array of the past 20 events on my github
+ *
+ * @param {number} pg current page number being requested
+ * @param {number} GITHUB_EVENTS_PER_PAGE set to 20
+ * @see: https://docs.github.com/en/rest/reference/activity#list-public-events-for-a-user
+ */
 const getGithubEvents = async (pg = 1, GITHUB_EVENTS_PER_PAGE) => {
   try {
     // adddlert('FAKE 403');
@@ -14,19 +21,23 @@ const getGithubEvents = async (pg = 1, GITHUB_EVENTS_PER_PAGE) => {
   } catch (e) {
     console.error(e);
   }
-  //  https://docs.github.com/en/rest/reference/activity#list-public-events-for-a-user
 };
+
+/**
+ * gets rich data associated with github commit event
+ *
+ * @param {string} url for request
+ * @see: https://docs.github.com/en/rest/reference/repos#get-a-commit
+ */
 
 const getCommit = async (url) => {
   try {
-    adddlert('FAKE 403');
     const res = await axios.get(url);
     console.log(`COMMIT`, res);
     return res.data;
   } catch (e) {
     console.error(e);
   }
-  // https://docs.github.com/en/rest/reference/repos#get-a-commit
 };
 
 export { getGithubEvents, getCommit };
